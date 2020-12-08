1f4209248d6a3899efd2cde5b2a2f0f200cebce4
YOKO <yoko.wang@makeblock.com>
2020-12-08 22:12:46 +0800

    feat: 新增首页

--- src/components/task-list/index.tsx
+++ src/components/task-list/index.tsx
@@ -7,23 +7,45 @@ export interface TaskListItemType {
   money: number;
   name: string;
   avatar: string;
+  tabs: string[];
 }
 
 export interface TaskListProps {
   data: TaskListItemType[];
 }
 
-const TaskListItem: React.FC = () => {
-  return <div className="task-list-item">ddddd</div>;
+const TaskListItem: React.FC<TaskListItemType> = ({ title, tabs, avatar, name, money }) => {
+  return (
+    <div className="task-list-item">
+      <div className="task-title">{title}</div>
+      <ul className="task-tabs-list">
+        {tabs.map((tab, index) => (
+          <li key={index}>
+            <span>{tab}</span>
+          </li>
+        ))}
+      </ul>
+      <div className="avatar-wrapper">
+        <img src={avatar} width={16} height={16} alt="" />
+        <span>{name}</span>
+      </div>
+      <div className="money-wrapper">
+        <div className="jinbi-wrapper">
+          <div className="jinbi"></div>
+          <div className="yongjin">佣金</div>
+        </div>
+        <div className="money">+{money}</div>
+      </div>
+    </div>
+  );
 };
 
-const TaskList: React.FC<TaskListProps> = props => {
-  const { data } = props;
+const TaskList: React.FC<TaskListProps> = ({ data }) => {
   return (
     <ul className="task-list">
-      {data.map((_: TaskListItemType, index: number) => (
+      {data.map((item: TaskListItemType, index: number) => (
         <li key={index}>
-          <TaskListItem key={index} />
+          <TaskListItem {...item} key={index} />
         </li>
       ))}
     </ul>
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,26 +1,75 @@
 import TaskList, { TaskListItemType } from '@/components/task-list';
 import React from 'react';
-import './index.css';
+import './index.less';
 
 const Home: React.FC = () => {
   const data: TaskListItemType[] = [
     {
       id: 0,
-      title: 'string',
+      title: '简单填码赚5元',
       money: 4,
-      name: 'string',
-      avatar: 'string',
+      name: '刘女士',
+      avatar:
+        'https://dev-res-cn.oss-cn-shenzhen.aliyuncs.com/next-maker/cms/9174d350-35fe-11eb-912f-c9d067017d00.jpg',
+      tabs: ['初级任务'],
     },
     {
       id: 1,
-      title: 'string',
+      title: '简单填码赚5元',
       money: 4,
-      name: 'string',
-      avatar: 'string',
+      name: '刘女士',
+      avatar:
+        'https://dev-res-cn.oss-cn-shenzhen.aliyuncs.com/next-maker/cms/9174d350-35fe-11eb-912f-c9d067017d00.jpg',
+      tabs: ['初级任务'],
+    },
+    {
+      id: 2,
+      title: '简单填码赚5元',
+      money: 4,
+      name: '刘女士',
+      avatar:
+        'https://dev-res-cn.oss-cn-shenzhen.aliyuncs.com/next-maker/cms/9174d350-35fe-11eb-912f-c9d067017d00.jpg',
+      tabs: ['初级任务'],
+    },
+    {
+      id: 3,
+      title: '简单填码赚5元',
+      money: 4,
+      name: '刘女士',
+      avatar:
+        'https://dev-res-cn.oss-cn-shenzhen.aliyuncs.com/next-maker/cms/9174d350-35fe-11eb-912f-c9d067017d00.jpg',
+      tabs: ['初级任务'],
+    },
+    {
+      id: 4,
+      title: '简单填码赚5元',
+      money: 4,
+      name: '刘女士',
+      avatar:
+        'https://dev-res-cn.oss-cn-shenzhen.aliyuncs.com/next-maker/cms/9174d350-35fe-11eb-912f-c9d067017d00.jpg',
+      tabs: ['初级任务'],
+    },
+    {
+      id: 5,
+      title: '简单填码赚5元',
+      money: 4,
+      name: '刘女士',
+      avatar:
+        'https://dev-res-cn.oss-cn-shenzhen.aliyuncs.com/next-maker/cms/9174d350-35fe-11eb-912f-c9d067017d00.jpg',
+      tabs: ['初级任务'],
     },
   ];
   return (
-    <div className="home">
+    <div className="home-page">
+      <div className="nav-wrapper">
+        <div className="nav-list">
+          <div className="nav-list-wrapper">
+            <div className="nav-list-item active">全部</div>
+            <div className="nav-list-item">最新</div>
+          </div>
+        </div>
+        <div className="search"></div>
+      </div>
       <TaskList data={data} />
     </div>
   );
