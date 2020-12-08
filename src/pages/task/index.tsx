6b9520ceb6153151c057c4030b3a3829a565c595
YOKO <yoko.wang@makeblock.com>
2020-12-08 22:35:28 +0800

    feat: 新增任务页面

--- src/pages/task/index.tsx
+++ src/pages/task/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import './index.less';
 
 const Task: React.FC = () => {
-  return <div className="task">task</div>;
+  return (
+    <div className="task-page">
+      <div className="nav-wrapper">
+        <div className="nav-list">
+          <div className="nav-list-wrapper">
+            <div className="nav-list-body">
+              <div className="nav-list-item active">全部</div>
+              <div className="nav-list-item">最新</div>
+              <div className="nav-list-item">最新</div>
+              <div className="nav-list-item">最新</div>
+              <div className="nav-list-item">最新</div>
+              <div className="nav-list-item">最新</div>
+              <div className="nav-list-item">最新</div>
+              <div className="nav-list-item">最新</div>
+              <div className="nav-list-item">最新</div>
+              <div className="nav-list-item">最新</div>
+            </div>
+          </div>
+        </div>
+        <div className="search"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Task;
