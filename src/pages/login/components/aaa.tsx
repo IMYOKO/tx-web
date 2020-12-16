d3bb58944760fcf0c7b59fbe08ac800301ee307a
YOKO <yoko.wang@makeblock.com>
2020-12-16 20:57:37 +0800

    feat: 删除无用的组件

--- src/pages/login/components/aaa.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const A = () => {
-  return <div className="aaa">aaa</div>;
-};
-
-export default A;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import A from './components/aaa';
 import './index.less';
 
 const Login = () => {
   return (
     <div className="login">
       <div className="bbb">bbb</div>
-      <A />
     </div>
   );
 };
